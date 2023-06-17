ef65dc0dda6f242e0783b0c95a3985b67767f654
Adir Noyman <ofahol2007@hotmail.com>
2023-06-17 19:47:03 +0300

    Added image upload functionality

--- app.js
+++ app.js
@@ -3,23 +3,35 @@ require('express-async-errors');
 
 const express = require('express');
 const app = express();
+const fileUpload = require('express-fileupload');
 
 // database
 const connectDB = require('./db/connect');
 
+// product router
+const productRouter = require('./routes/productRoutes');
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+// set the folder files we want to be publicley available
+app.use(express.static('./public'));
+// load the request body from the request object
+app.use(express.json());
+// load the file sent in the request object
+app.use(fileUpload());
 
 app.get('/', (req, res) => {
   res.send('<h1>File Upload Starter</h1>');
 });
 
+app.use('/api/v1/products', productRouter);
+
 // middleware
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 const start = async () => {
   try {
---
+++ controllers/uploadsController.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const { StatusCodes } = require('http-status-codes');
+
+const uploadProductImage = async (req, res) => {
+  const productImage = req.files.image;
+  const imagePath = path.join(
+    __dirname,
+    '../public/uploaded_images/' + `${productImage.name}`
+  );
+  // move the uploaded product image to the the folder we want on the server
+  await productImage.mv(imagePath);
+  res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploaded_images/${productImage.name}` } });
+};
+
+module.exports = {
+  uploadProductImage,
+};
